efa00388b77a054e6470e9377bbe4a1618f35620
lorenz.bayr <lorenz.bayr@virtual7.de>
2020-05-18 11:38:35 +0200

    configured karma.conf.js with pact

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,25 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const path = require('path');
+
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular', 'pact'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@pact-foundation/karma-pact')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-getting-started'),
+      dir: path.join(__dirname, './coverage/angular-getting-started'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
@@ -27,6 +30,16 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['ChromeHeadless'],
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    pact: [{
+      cors: true,
+      spec: 3,
+      port: 1234,
+      log: path.resolve(process.cwd(), './pact/logs', 'mockserver-integration.log'),
+      dir: path.resolve(process.cwd(), './pact/pacts')
+    }],
+    proxies: {
+      '/api': 'http://127.0.0.1:1234/api'
+    }
   });
 };
